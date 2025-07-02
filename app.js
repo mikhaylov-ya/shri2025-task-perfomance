6b6f2acd46460f595b17243aa790eb16cfa5dfbd
Yaroslav Mikhaylov <sponaproz@gmail.com>
2025-07-02 21:21:52 +0300

    fix js

--- app.js
+++ app.js
@@ -350,4 +350,4 @@ function Main() {
 setTimeout(() => {
   const root = ReactDOM.createRoot(document.getElementById('app'));
   root.render(/*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Header, null), /*#__PURE__*/React.createElement(Main, null)));
-}, 10);
+}, 100);
